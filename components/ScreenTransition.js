d725bb3e8e9f09fca0e20ced077654360dfc3621
EchoZenith <60392923+EchoZenith@users.noreply.github.com>
2026-04-29 16:27:43 +0800

    refactor: remove slide-up animation, keep only fade-in for screen transitions

--- components/ScreenTransition.js
+++ components/ScreenTransition.js
@@ -3,23 +3,14 @@ import { Animated, Platform } from 'react-native';
 
 export default function ScreenTransition({ children, id }) {
   const fadeAnim = useRef(new Animated.Value(0)).current;
-  const slideAnim = useRef(new Animated.Value(20)).current;
 
   useEffect(() => {
     fadeAnim.setValue(0);
-    slideAnim.setValue(20);
-    Animated.parallel([
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 200,
-        useNativeDriver: Platform.OS !== 'web',
-      }),
-      Animated.timing(slideAnim, {
-        toValue: 0,
-        duration: 200,
-        useNativeDriver: Platform.OS !== 'web',
-      }),
-    ]).start();
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: Platform.OS !== 'web',
+    }).start();
   }, [id]);
 
   return (
@@ -27,7 +18,6 @@ export default function ScreenTransition({ children, id }) {
       style={{
         flex: 1,
         opacity: fadeAnim,
-        transform: [{ translateY: slideAnim }],
       }}
     >
       {children}
